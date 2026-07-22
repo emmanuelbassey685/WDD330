96d042459ed4cc3751d6636b0ce66abd5588716c
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-07-22 19:09:02 +0300

    W04 Team Activity Checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,25 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
+
+const checkout = new CheckoutProcess("so-cart", "#order-summary");
+    checkout.init();
+
+const zip = document.querySelector("input[name='zip']");
+    zip.addEventListener("blur", () => { checkout.calculateOrderTotal();
+});
+
+const form = document.querySelector("#checkoutForm");
+
+    form.addEventListener("submit", async (e) => { e.preventDefault();
+
+    if (!form.checkValidity()) {
+        form.reportValidity();
+
+        return;
+    }
+
+    await checkout.checkout(form);
+
+});
